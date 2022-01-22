96b0dd30f4e9966c64d7acc2d1424bd8542a6388
thanhauco <thanhauco@gmail.com>
2022-01-22 00:33:42 +0000

    feat(events): define Order and Saga lifecycle domain event models

---
+++ pkg/events/domain_events.go
@@ -0,0 +1,23 @@
+package events
+
+type OrderCreatedEvent struct {
+	OrderID    string  `json:"order_id"`
+	CustomerID string  `json:"customer_id"`
+	Amount     float64 `json:"amount"`
+	Currency   string  `json:"currency"`
+	SKU        string  `json:"sku"`
+	Quantity   int     `json:"quantity"`
+}
+
+type PaymentReservedEvent struct {
+	SagaID        string  `json:"saga_id"`
+	TransactionID string  `json:"transaction_id"`
+	Amount        float64 `json:"amount"`
+}
+
+type InventoryReservedEvent struct {
+	SagaID      string `json:"saga_id"`
+	WarehouseID string `json:"warehouse_id"`
+	SKU         string `json:"sku"`
+	Quantity    int    `json:"quantity"`
+}
