a5ff6da5d89aed4f3417c1ebf5196fbf58d66cc3
thanhauco <thanhauco@gmail.com>
2022-02-18 02:32:21 +0000

    feat(idempotency): define IdempotencyRecord and interface contracts

---
+++ pkg/idempotency/record.go
@@ -0,0 +1,19 @@
+package idempotency
+
+import "time"
+
+type RecordStatus string
+
+const (
+	StatusProcessing RecordStatus = "PROCESSING"
+	StatusCompleted  RecordStatus = "COMPLETED"
+	StatusFailed     RecordStatus = "FAILED"
+)
+
+type Record struct {
+	Key       string       `json:"key"`
+	Status    RecordStatus `json:"status"`
+	Response  string       `json:"response"`
+	CreatedAt time.Time    `json:"created_at"`
+	ExpiresAt int64        `json:"expires_at"`
+}
