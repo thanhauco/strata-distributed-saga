c18f23dd7d501e3eda85ec126dbcbfc260657481
thanhauco <thanhauco@gmail.com>
2022-02-25 20:14:49 +0000

    feat(idempotency): scaffold DynamoDB conditional write idempotency repository

---
+++ pkg/idempotency/dynamo_store.go
@@ -0,0 +1,18 @@
+package idempotency
+
+import (
+	"context"
+	"time"
+)
+
+type DynamoStore struct {
+	tableName string
+}
+
+func NewDynamoStore(tableName string) *DynamoStore {
+	return &DynamoStore{tableName: tableName}
+}
+
+func (d *DynamoStore) KeyHash(sagaID, stepName string) string {
+	return sagaID + ":" + stepName
+}
