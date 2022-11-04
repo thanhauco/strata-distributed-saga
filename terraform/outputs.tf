1618fb1c293a3001b90251eaebb2c604aa2c9f61
thanhauco <thanhauco@gmail.com>
2022-11-04 07:45:10 +0000

    feat(terraform): declare Terraform output variables for endpoints and ARNs

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "saga_state_machine_arn" {
+  value = aws_sfn_state_machine.order_saga.arn
+}
+
+output "event_bus_name" {
+  value = aws_cloudwatch_event_bus.saga_bus.name
+}
+
+output "dlq_url" {
+  value = aws_sqs_queue.saga_dlq.url
+}
